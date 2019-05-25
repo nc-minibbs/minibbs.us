6bb15d77c567dcf03e69d3b4bb1e02246faf90ab
bsaul <bradleysaul@gmail.com>
2019-05-25 13:44:31 -0400

    updates website: attempting to get links to work

--- render_site.R
+++ render_site.R
@@ -1,2 +1,9 @@
+#------------------------------------------------------------------------------#
+#   TITLE: Render the minibbs.us website 
+#    DATE: 20190525
+#    PROG: B. Saul
+#    DESC: Render the minibbs.us website from .Rmd files
+#------------------------------------------------------------------------------#
 
 rmarkdown::render_site()
+  
