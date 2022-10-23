aed3a2496abd7aa0f0da2d7ca4b38182de77438f
Bradley Saul <bradleysaul@fastmail.com>
2022-10-22 20:13:41 -0400

    add nix support (#16)

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A flake for the minibbs.us site ";
+  nixConfig = {
+    bash-prompt = "mbbs> ";
+  };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default =  pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [
+          pkgs.R
+        ];
+      }; 
+
+    });
+}
+
