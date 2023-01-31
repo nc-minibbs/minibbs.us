ce951684382e5d88ba92dd4971fe7125cc4cdbc5
Bradley Saul <bradleysaul@fastmail.com>
2023-01-30 20:41:21 -0500

    start into using bootstrap

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,15 +18,21 @@ pandoc site/index.md \
   --from=markdown \
   --to=html \
   --output=$OUTDIR/index.html \
+  --template=site/template.html \
   --include-in-header=site/import-vega.html \
   --include-in-header=site/import-elm.html \
+  --include-before-body=site/navbar.html \
   --include-after-body=site/index.js.html \
+  --css=bootstrap.min.css \
   --standalone
 
 pandoc site/results/index.md \
   --from=markdown \
   --to=html \
   --output=$OUTDIR/results/index.html \
+  --template=site/template.html \
+  --include-before-body=site/navbar.html \
+  --css=bootstrap.min.css \
   --standalone
 
 
@@ -34,19 +40,26 @@ pandoc site/results/individual-species.md \
   --from=markdown \
   --to=html \
   --output=$OUTDIR/results/individual-species.html \
+  --template=site/template.html \
   --include-in-header=site/import-vega.html \
   --include-in-header=site/results/individual-species-elm.html \
+  --include-before-body=site/navbar.html \
   --include-after-body=site/results/individual-species.js.html \
+  --css=bootstrap.min.css \
   --standalone
 
 pandoc site/results/traits.md \
   --from=markdown \
   --to=html \
   --output=$OUTDIR/results/traits.html \
+  --template=site/template.html \
   --include-in-header=site/import-vega.html \
   --include-in-header=site/results/traits-elm.html \
+  --include-before-body=site/navbar.html \
   --include-after-body=site/results/traits.js.html \
+  --css=bootstrap.min.css \
   --standalone
 
 
 cp -r data/ $OUTDIR
+cp site/bootstrap.min.css $OUTDIR
