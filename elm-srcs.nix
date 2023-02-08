1959be9c826c095244683f0c724c930edd69fc49
Bradley Saul <bradleysaul@fastmail.com>
2023-02-08 08:11:18 -0500

    initial pass at defining a nix package

---
+++ elm-srcs.nix
@@ -0,0 +1,147 @@
+{
+
+      "Confidenceman02/elm-select" = {
+        sha256 = "03lx3w4x47i51x7kdzik0c3565mi5jqdpgzqakx9rgsw02v7vs05";
+        version = "8.2.0";
+      };
+
+      "Orasund/elm-ui-widgets" = {
+        sha256 = "0w6bbvc11gc80w737rxpands8jsyad8lr8gwg49x6vkjf365wqpk";
+        version = "3.4.0";
+      };
+
+      "elm/browser" = {
+        sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
+        version = "1.0.2";
+      };
+
+      "elm/core" = {
+        sha256 = "19w0iisdd66ywjayyga4kv2p1v9rxzqjaxhckp8ni6n8i0fb2dvf";
+        version = "1.0.5";
+      };
+
+      "elm/html" = {
+        sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
+        version = "1.0.0";
+      };
+
+      "elm/json" = {
+        sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
+        version = "1.1.3";
+      };
+
+      "gicentre/elm-vegalite" = {
+        sha256 = "1d3n7gd1ga99b8dv1j7y9xi4qfmjf4rbs6j01mj8gm5y4k3rygy6";
+        version = "5.3.0";
+      };
+
+      "mdgriffith/elm-ui" = {
+        sha256 = "0ffcqv4a4ad400hwp824m3qq4jy82cqp5ghmhp1m0q7n004z6kgv";
+        version = "1.1.8";
+      };
+
+      "rtfeldman/elm-css" = {
+        sha256 = "0mffazjks3wmsn0f0xs8gs6zg39ahipz0hqif2y7qphzjdq6h6yh";
+        version = "17.1.1";
+      };
+
+      "uncover-co/elm-widgets" = {
+        sha256 = "0igwirnxjccrzj4378h998xvfwaxs6gxlz44pcj6a9dj10hg4wv3";
+        version = "2.0.0";
+      };
+
+      "avh4/elm-color" = {
+        sha256 = "0n16wnvp87x9az3m5qjrl6smsg7051m719xn5d244painx8xmpzq";
+        version = "1.0.0";
+      };
+
+      "dzuk-mutant/elm-html-styled-aria" = {
+        sha256 = "1gwk45zspfkdgxzj27sgnilirydj5kzbmdhrphfcpf1p1s1kighr";
+        version = "1.0.1";
+      };
+
+      "elm-community/intdict" = {
+        sha256 = "09i1fk63gp6sr6kc6ccs8g0kxvqhw5czghi9cl8flizanrgcmva1";
+        version = "3.0.0";
+      };
+
+      "elm-community/list-extra" = {
+        sha256 = "02grd0p5hc2gvdy4n723d1s28pm1grn95jrzic6jcgb26qh16vcc";
+        version = "8.7.0";
+      };
+
+      "elm/parser" = {
+        sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
+        version = "1.1.0";
+      };
+
+      "elm/regex" = {
+        sha256 = "0lijsp50w7n1n57mjg6clpn9phly8vvs07h0qh2rqcs0f1jqvsa2";
+        version = "1.0.0";
+      };
+
+      "elm/svg" = {
+        sha256 = "1cwcj73p61q45wqwgqvrvz3aypjyy3fw732xyxdyj6s256hwkn0k";
+        version = "1.0.1";
+      };
+
+      "elm/time" = {
+        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+        version = "1.0.0";
+      };
+
+      "elm/url" = {
+        sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
+        version = "1.0.0";
+      };
+
+      "elm/virtual-dom" = {
+        sha256 = "1yvb8px2z62xd578ag2q0r5hd1vkz9y7dfkx05355iiy1d7jwq4v";
+        version = "1.0.3";
+      };
+
+      "fredcy/elm-parseint" = {
+        sha256 = "087c7p9m607p4pb9aziln3mqfczvkfm50fna5sxcsjlj45x7ibp6";
+        version = "2.0.1";
+      };
+
+      "justinmimbs/date" = {
+        sha256 = "0fpci4mvnjdjl3gyjamw39virj3x38s0dsgrm8sy94qwwn2dlds3";
+        version = "4.0.1";
+      };
+
+      "justinmimbs/time-extra" = {
+        sha256 = "1qd9cpwpjj944rijdyfflwsl0qp6f492q6hj9pm3xfbwiq8kdjhn";
+        version = "1.1.1";
+      };
+
+      "noahzgordon/elm-color-extra" = {
+        sha256 = "1km72jlmgg5iqxpnvzry9wavjlwcs0n3ks8wwdiy6rj2rmzm1z20";
+        version = "1.0.2";
+      };
+
+      "robinheghan/murmur3" = {
+        sha256 = "15asmgr2zqh7rkywrg5647rpdqkpzxk02v5qc6ndj60jza3gsmjk";
+        version = "1.0.0";
+      };
+
+      "rtfeldman/elm-hex" = {
+        sha256 = "1y0aa16asvwdqmgbskh5iba6psp43lkcjjw9mgzj3gsrg33lp00d";
+        version = "1.0.0";
+      };
+
+      "tesk9/palette" = {
+        sha256 = "03kalsf6izwwawvh3n04ng3xnj6kdnfnv5l0qdfii4hls8cjc8pl";
+        version = "3.0.1";
+      };
+
+      "turboMaCk/queue" = {
+        sha256 = "0d3py1wjfdnyphbnxzc0467lpqcym1r11sns7mcj021x1g1fspi1";
+        version = "1.1.0";
+      };
+
+      "uncover-co/elm-theme" = {
+        sha256 = "0vhphraxgrfvyv0jy5wb558vy0qb6sp58bf7d6rxzcf9l44czh4l";
+        version = "2.0.0";
+      };
+}
--- flake.nix
+++ flake.nix
@@ -12,6 +12,67 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+
+
+      packages = {
+        # The elm bits were hacked from the code that
+        #  elm2nix init produces
+        site = pkgs.stdenv.mkDerivation {
+          name = "site";
+          src = ./.;
+
+          buildInputs = [
+            pkgs.elmPackages.elm
+            pkgs.pandoc
+          ];
+
+          buildPhase = pkgs.elmPackages.fetchElmDeps {
+            elmPackages = import ./elm-srcs.nix;
+            elmVersion = "0.19.1";
+            registryDat = ./registry.dat;
+          };
+
+          installPhase =
+          let elmModules = 
+            [ "DisplayTraits"
+              "DisplayIndividualSpecies"
+              "Home"
+            ];
+            pages = 
+            [ "index"
+            ];
+          in 
+          ''
+          mkdir -p $out/_site/js
+
+          ${pkgs.lib.concatStrings (map (module : 
+           '' 
+            echo "compiling elm modules"
+            ${pkgs.elmPackages.elm}/bin/elm make src/${module}.elm \
+                --output=$out/_site/js/${module}.js \
+                --optimize
+           '') elmModules ) 
+          
+           }
+
+          ${pkgs.pandoc}/bin/pandoc site/index.md \
+              --from=markdown \
+              --to=html \
+              --output=$out/index.html \
+              --template=site/template.html \
+              --include-in-header=site/import-vega.html \
+              --include-in-header=site/import-elm.html \
+              --include-before-body=site/navbar.html \
+              --include-after-body=site/index.js.html \
+              --css=/css/style.css \
+              --css=/css/bootstrap.min.css \
+              --standalone
+          '';
+        };
+      };
+
+      packages.default = self.packages.${system}.site;
+
       devShells.default =  pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
         buildInputs = [
@@ -23,6 +84,8 @@
           pkgs.pandoc
 
           pkgs.updog # simple HTTP server for running HTTP server locally (for testing)
+          
+          pkgs.elm2nix
           pkgs.elmPackages.elm
           pkgs.elmPackages.elm-format
           pkgs.elmPackages.elm-analyse
